72e9a7239e18c7b767d70de62639d475a71062ce
ZupperLeo <leonardo.araujo@zup.com.br>
2021-08-09 09:19:44 -0300

    Configuracao da JPA, injecao de dependencia pelo construtor

--- src/main/java/br/com/alura/leilao/dao/UsuarioDao.java
+++ src/main/java/br/com/alura/leilao/dao/UsuarioDao.java
@@ -1,8 +1,8 @@
 package br.com.alura.leilao.dao;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.leilao.model.Usuario;
@@ -10,8 +10,13 @@ import br.com.alura.leilao.model.Usuario;
 @Repository
 public class UsuarioDao {
 
-	@PersistenceContext
 	private EntityManager em;
+
+	/*Utilizar injeção de dependencia no construtor facilita na escrita dos testes*/
+	@Autowired
+	public UsuarioDao(EntityManager em) {
+		this.em = em;
+	}
 
 	public Usuario buscarPorUsername(String username) {
 		return em.createQuery("SELECT u FROM Usuario u WHERE u.nome = :username", Usuario.class)
